2c85219b0dd3ab6a8c5abef78c21431809452303
AJWebCreation <anthonyjnicolaou0@gmail.com>
2026-05-24 15:42:45 +0100

    Delete find.cjs

--- find.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('src/App.tsx', 'utf-8');
-const regex = /onClick=\{([\s\S]*?)\}/g;
-let match;
-while ((match = regex.exec(content)) !== null) {
-  let inner = match[1].trim();
-  if (inner.includes('\n')) inner = inner.split('\n')[0] + ' ...';
-  console.log(inner);
-}
